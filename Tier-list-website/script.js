a330214bae56ff4f6099e1c457472e97fe74f45b
chaoDylan2025 <114248884+chaoDylan2025@users.noreply.github.com>
2024-08-08 20:56:36 -0400

    modified tier list structure and implemented image dragging feature

--- Tier-list-website/script.js
+++ Tier-list-website/script.js
@@ -1,56 +1,47 @@
-const num_of_images_in_row = [];
-
 function createTierList(){
     document.getElementById("default_tier_list").innerHTML = 
     `<div class="tier">
         <div class="tier-name" id="S">
             <span> S </span>
         </div>
-        <div class="image-container">
-        </div>
+        <div class="image-container"></div>
     </div>
     <div class="tier">
         <div class="tier-name" id="A">
             <span> A </span>
         </div>
-        <div class="image-container">
-        </div>
+        <div class="image-container"></div>
     </div>
     <div class="tier">
         <div class="tier-name" id="B">
             <span> B </span>
         </div>
-        <div class="image-container">
-        </div>
+        <div class="image-container"></div>
     </div>
     <div class="tier">
         <div class="tier-name" id="C">
             <span> C </span>
         </div>
-        <div class="image-container">
-        </div>
+        <div class="image-container"></div>
     </div>
     <div class="tier">
         <div class="tier-name" id="D">
             <span> D </span>
         </div>
-        <div class="image-container">
-        </div>
+        <div class="image-container"></div>
     </div>
     <div class="tier">
         <div class="tier-name" id="F">
             <span> F </span>
         </div>
-        <div class="image-container">
-        </div>
+        <div class="image-container"></div>
     </div>`;
 
-    const tiers = document.querySelectorAll('.tier');
+    const tiers = document.querySelectorAll('.image-container');
     tiers.forEach(tier => {
         tier.addEventListener('dragover', dragOver);
         tier.addEventListener('drop', drop);
     });
-    console.log(num_of_images_in_row);
 }
 
 function dragStart(event) {
@@ -64,16 +55,37 @@ function dragOver(event) {
 function drop(event) {
     event.preventDefault();
     const src = event.dataTransfer.getData('text/plain');
-    const image = new Image(100.00, 100.00);
+    const image = new Image();
+    image.style.width = '85px';
+    image.style.height = 'auto';
+
     image.src = src;
+    image.draggable = true;
+
+    image.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("text/plain", e.target.src);
+    });
+
+    const from_image_container = document.querySelector(`#place-holder img[src="${image.src}"]`);
+    if(from_image_container){
+        from_image_container.remove();
+    }
+    else{
+        const tiers = document.querySelectorAll('.image-container');
+        tiers.forEach(tier => {
+            const from_image_container = document.querySelector(`.image-container img[src="${image.src}"]`);
+            if(from_image_container){
+                from_image_container.remove();
+            }
+        });
+    }
     event.target.appendChild(image);
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    const tiers = document.querySelectorAll('.tier');
+    const tiers = document.querySelectorAll('.image-container');
     tiers.forEach(tier => {
         tier.addEventListener('dragover', dragOver);
         tier.addEventListener('drop', drop);
     });
-    console.log(num_of_images_in_row);
 });
