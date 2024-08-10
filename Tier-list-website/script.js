656bbf5cf7227c9e1c160aa09a0fab56925c79ef
chaoDylan2025 <114248884+chaoDylan2025@users.noreply.github.com>
2024-08-10 14:40:13 -0400

    implemented image rearrangement and modified tier list structure code

--- Tier-list-website/script.js
+++ Tier-list-website/script.js
@@ -55,14 +55,21 @@ function dragOver(event) {
 function drop(event) {
     event.preventDefault();
 
+    // Ensure that the target is the correct image container
+    var image_in_container = event.target;
+    if (!image_in_container.classList.contains('image-container')) {
+        image_in_container = image_in_container.closest('.image-container');
+    }
+
     // Get url of image that is being dragged
     const src = event.dataTransfer.getData('text/plain');
 
     // Creates image element of image being dragged
     const image = new Image();
     image.style.width = '85px';
-    image.style.height = 'auto';
+    image.style.height = '85px';
     image.src = src;
+    image.style.objectFit = 'cover';
     image.draggable = true;
 
     // Sets dragged data to image's src attribute
@@ -70,20 +77,40 @@ function drop(event) {
         e.dataTransfer.setData("text/plain", e.target.src);
     });
 
-    // Remove selected images from tier list image container if those images are dragged into any tiers
+    // Remove selected image(s) from previous location
     const from_image_container = document.querySelector(`#place-holder img[src="${image.src}"]`);
     if(from_image_container){
         from_image_container.remove();
-    }
-    // Remove selected images from tiers if selected images are moved into different tiers
-    else{
+    } 
+    else {
         const tiers = document.querySelectorAll('.image-container');
         tiers.forEach(tier => {
-            const from_image_container = document.querySelector(`.image-container img[src="${image.src}"]`);
-            if(from_image_container){
-                from_image_container.remove();
+            const existing_image = tier.querySelector(`img[src="${image.src}"]`);
+            if(existing_image){
+                existing_image.remove();
             }
         });
     }
-    event.target.appendChild(image);
+    
+    /** Rearranging images in a tier */
+    const all_images = image_in_container.querySelectorAll('img');
+    var inserted_image = false;
+
+    // Iterate through all images to properly rearrange images
+    for(var i = 0; i < all_images.length; i++){
+        const current_image = all_images[i];
+        const current_rect = current_image.getBoundingClientRect();
+
+        // Insert image to the left if current image is being moved to the left
+        if(event.clientX < current_rect.left + current_rect.width / 2){
+            image_in_container.insertBefore(image, current_image);
+            inserted_image = true;
+            break;
+        }
+    }
+
+    // Append image if no images are being moved to the left
+    if(!inserted_image){
+        image_in_container.appendChild(image);
+    }
 }
