1fa24e63c563326fa6f795e403c3fc8066ae7b49
chaoDylan2025 <chaod@mail.gvsu.edu>
2024-12-18 19:37:11 -0500

    created an initial home page

--- Tier-list-website/Tier-List-Clone-Website/src/App.vue
+++ Tier-list-website/Tier-List-Clone-Website/src/App.vue
@@ -2,5 +2,35 @@
 </script>
 
 <template>
+    <v-app>
+        <v-container>
+        </v-container>
+        <v-container>
+            <v-row>
+                <v-col>
+                </v-col>
+                <v-col>
+                    <div>
+                        <p>Welcome to Tier List Clone!</p>
+                    </div>
+                </v-col>
+                <v-col>
+                </v-col>
+            </v-row>
+            <v-row class="">
+                <v-col>
+                    <v-btn>Create a Tier List</v-btn>
+                </v-col>
+                
+                <v-col>
+                    <v-spacer></v-spacer>
+                </v-col>
+    
+                <v-col>
+                    <v-btn>Use a Default Tier List</v-btn>
+                </v-col>                 
+            </v-row>
 
+        </v-container>
+    </v-app>
 </template>
--- Tier-list-website/Tier-List-Clone-Website/src/main.js
+++ Tier-list-website/Tier-List-Clone-Website/src/main.js
@@ -1,11 +1,30 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createVuetify } from "vuetify"
+import { aliases, mdi } from "vuetify/iconsets/mdi"
+import { fa } from "vuetify/iconsets/fa"
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 import App from './App.vue'
 import router from './router'
+
+const vuetify = createVuetify({
+    icons: {
+      defaultSet: "mdi",
+      aliases,
+      sets: { mdi, fa },
+    },
+  
+    components,
+    directives,
+})
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
