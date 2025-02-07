19071af33e78233c9f88ccb6f62fb32f0fc6c955
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-02-07 14:11:47 -0500

    setting up pinia

--- Tier-list-website/Tier-List-Clone-Website/src/main.js
+++ Tier-list-website/Tier-List-Clone-Website/src/main.js
@@ -3,14 +3,20 @@ import { createVuetify } from "vuetify"
 import { aliases, mdi } from "vuetify/iconsets/mdi"
 import { fa } from "vuetify/iconsets/fa"
 import VueDraggable from 'vuedraggable'
-import "vuetify/styles";
-import "@mdi/font/css/materialdesignicons.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+import "vuetify/styles"
+import "@mdi/font/css/materialdesignicons.css"
+import "@fortawesome/fontawesome-free/css/all.css"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
 
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const app = createApp(App)
+
+const pinia = createPinia()
 
 const vuetify = createVuetify({
     icons: {
@@ -22,10 +28,9 @@ const vuetify = createVuetify({
     directives,
 })
 
-const app = createApp(App)
-
 app.use(router)
 app.use(VueDraggable)
 app.use(vuetify)
+pinia.use(piniaPluginPersistedstate)
 
 app.mount('#app')
