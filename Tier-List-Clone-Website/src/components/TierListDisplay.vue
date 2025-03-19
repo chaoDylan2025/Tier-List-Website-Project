35ba8ac811f89aa5d5bc78f2e4d330ef987d685f
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-03-19 11:03:03 -0400

    refactored some code from CustomizationScreen and TierListDisplay

--- Tier-List-Clone-Website/src/components/TierListDisplay.vue
+++ Tier-List-Clone-Website/src/components/TierListDisplay.vue
@@ -1,16 +1,18 @@
 <script setup>
 import draggable from 'vuedraggable'
 import { ref, watch } from 'vue'
+import { uploadToImageContainer } from '../front-end-code/customize_screen_functions.js'
 
 const emit = defineEmits(['open_tier_name_mod', 'open_tier_color_mod', 'delete_tiers', 'update_tier_images'])
 const props = defineProps({
     tier_list: Object,
-    image_list: Array,
-    current_tier_images: Array,
+    files_arr: Array,
     show_mod_buttons: Boolean,
     show_checkboxes: Boolean,
-    delete_tiers_arr: Array
 })
+
+var current_tier_list = ref(props.tier_list)
+var files_arr = ref(props.files_arr)
 
 // Executes emit event when user clicks on button that modifies selected tier's name
 function open_tier_name_mod_dialog(index, props){
@@ -40,7 +42,7 @@ watch (() => tiers_to_delete_arr.value, (new_arr) => {
     <!-- Contains tier list structure -->
     <v-container class="ml-5 mt-10 d-print-inline px-10 mb-10 h-auto">
         <!-- Iterates through the tier list -->
-        <v-row class="d-flex flex-row" v-for="(tier, index) in props.tier_list" :key="tier.tier_name" :class="`d-print-flex h-auto w-100 overflow-hidden`">
+        <v-row class="d-flex flex-row" v-for="(tier, index) in current_tier_list" :key="tier.tier_name" :class="`d-print-flex h-auto w-100 overflow-hidden`">
             <!-- Displays when user wants to delete any tier(s) -->
             <div class="mr-5" v-if="props.show_checkboxes === true">
                 <v-row>
@@ -88,6 +90,32 @@ watch (() => tiers_to_delete_arr.value, (new_arr) => {
             </v-col>
         </v-row>
     </v-container>
+
+    <v-container id="image-place-holder-area">
+        <div>
+            <p id="cap_for_user"> Insert your images or uploaded images here </p>
+        </div> 
+        <div class="d-flex flex-column" id="image-place-holder">
+            <div class="mt-3 mb-3">
+                <div class="ml-2">
+                    <v-btn @click="uploadToImageContainer()" size="small"> Upload </v-btn>
+                </div>  
+            </div>
+            <div class="d-flex text-start justify-start align-start h-100 w-100" id="place-holder">
+                <draggable
+                v-model="files_arr"
+                group="tier_list"
+                tag="transition-group"
+                item-key="id">
+                    <template #item="{ element }" :key="element.id">
+                        <div>
+                            <img :src="element.src" height="85" width="85"></img>   
+                        </div>
+                    </template>
+                 </draggable>
+            </div>
+        </div>  
+    </v-container> 
 </template>
 
 <style lang="css">
@@ -96,5 +124,18 @@ watch (() => tiers_to_delete_arr.value, (new_arr) => {
     border-style: solid;
     border-color: black;
     border-width: 2px;
+}
+/** Styling for image container that contains images to be inserted in tier list */
+#image-place-holder{
+    background-color: rgba(40, 40, 40, 0.927);
+    border-style: solid;
+}
+#place-holder{
+    background-color: rgba(27, 27, 27, 0.927);
+    border-style: solid;
+    border-width: 1px;
+}
+#place-holder img{
+    width: 85px;
 }
 </style>
--- Tier-List-Clone-Website/src/views/CustomizeScreen.vue
+++ Tier-List-Clone-Website/src/views/CustomizeScreen.vue
@@ -5,7 +5,7 @@ import TierListDisplay from '../components/TierListDisplay.vue'
 import ModifyTierList from '../components/ModifyTierList.vue'
 import DeleteTiers from '../components/DeleteTiers.vue'
 import { open_modal_dialog, delete_tiers_modal_dialog, files_arr } from '../front-end-code/customize_screen_functions'
-import { add_new_tier, updateTierList, deleteTiers, uploadToImageContainer } from '../front-end-code/customize_screen_functions'
+import { add_new_tier, updateTierList, deleteTiers } from '../front-end-code/customize_screen_functions'
 
 const props = defineProps({
     tier_list: Object
@@ -30,51 +30,7 @@ var current_tier_list = ref(props.tier_list)
             </v-row>
         </v-container>
 
-        <TierListDisplay :tier_list="current_tier_list"  @update_tier_images="update_tier_images_arr"/>
-
-        <v-container id="image-place-holder-area">
-            <div>
-                <p id="cap_for_user"> Insert your images or uploaded images here </p>
-            </div> 
-            <div class="d-flex flex-column" id="image-place-holder">
-                <div class="mt-3 mb-3">
-                    <div class="ml-2">
-                        <v-btn @click="uploadToImageContainer()" size="small"> Upload </v-btn>
-                    </div>  
-                </div>
-                <div class="d-flex text-start justify-start align-start h-100 w-100" id="place-holder">
-                    <draggable
-                    v-model="files_arr"
-                    group="tier_list"
-                    tag="transition-group"
-                    item-key="id">
-                        <template #item="{ element }" :key="element.id">
-                            <div>
-                                <img :src="element.src" height="85" width="85"></img>   
-                            </div>
-                        </template>
-                    </draggable>
-                </div>
-            </div>  
-        </v-container> 
-
-        <!-- Contains trash icon that deletes images -->
-        <v-container>
-            <v-row></v-row>
-            <v-row>
-                <v-col cols="10"></v-col>
-                <v-col>
-                    <v-hover>
-                        <template v-slot:default="{ isHovering, props }">
-                            <v-card width="25" v-bind="props" :color="isHovering ? 'primary' : 'black'">
-                                <v-icon :icon="`mdi-delete`"></v-icon>
-                            </v-card>
-                        </template>
-                    </v-hover>
-                </v-col>
-            </v-row>
-
-        </v-container>
+        <TierListDisplay :tier_list="current_tier_list"  :files_arr="files_arr" @update_tier_images="update_tier_images_arr"/>
 
         <!-- Displays if user wants to customize tier list -->
         <v-dialog
@@ -93,26 +49,3 @@ var current_tier_list = ref(props.tier_list)
             @deleteTiers="deleteTiers"/>
     </v-app>
 </template>
-
-<style lang="css">
-/* Styling for Default Tier List border */
-.tier-border{
-    border-style: solid;
-    border-color: black;
-    border-width: 2px;
-}
-
-/** Styling for image container that contains images to be inserted in tier list */
-#image-place-holder{
-    background-color: rgba(40, 40, 40, 0.927);
-    border-style: solid;
-}
-#place-holder{
-    background-color: rgba(27, 27, 27, 0.927);
-    border-style: solid;
-    border-width: 1px;
-}
-#place-holder img{
-    width: 85px;
-}
-</style>
