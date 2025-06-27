2f4687a119ca8d3fb1a94cab58334f7d70ae5e42
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-06-26 23:12:59 -0400

    removed big space gap below uploaded images container

--- src/views/CustomizeScreen.vue
+++ src/views/CustomizeScreen.vue
@@ -72,7 +72,9 @@ onMounted(() => {
         </v-dialog>
 
         <!-- Displays if user wants to delete any tier(s) -->
-        <DeleteTiers :tier_delete_dialog="delete_tiers_modal_dialog" :tier_list="default_tier_list" @close="(state) =>  delete_tiers_modal_dialog = state"
+         <v-container v-show="false">
+            <DeleteTiers :tier_delete_dialog="delete_tiers_modal_dialog" :tier_list="default_tier_list" @close="(state) =>  delete_tiers_modal_dialog = state"
             @deleteTiers="deleteTiers"/>
+         </v-container>
     </v-app>
 </template>
